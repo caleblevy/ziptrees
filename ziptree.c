11871dba6aceda4393ef242c18d0d02a55d74e6d
Caleb Levy <clevy@intertrust.com>
2018-11-14 14:39:46 -0800

    Add first scratch file for ziptree.c

---
+++ ziptree.c
@@ -0,0 +1,26 @@
+// Initial toy implementation of zip tree in c
+
+#include <stdio.h>
+
+typedef struct tree_node Tree;
+struct node {
+    Tree * left, * right;
+    int key;
+    unsigned int rank;
+};
+
+
+Tree * search(int k, Tree * T) {
+    
+}
+
+Tree * insert_zip(int k, Tree * root) {
+    Tree * new
+}
+
+
+void main() {
+    Tree * root;
+    int i;
+    printf("size = %d\n", 1);
+}
